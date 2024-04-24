7f103456a79bf5efb7c01d444249f3fe509837eb
Ken <kensanchez16@gmail.com>
2024-04-23 18:46:24 -0600

    User models validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :complete_name, :birth_date, :phone, presence: true
+  validates :email, :username, uniqueness: true
 end
