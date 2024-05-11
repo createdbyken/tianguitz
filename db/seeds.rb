de792f7b2010b209b0dba0fa9d37e24b8d3ee72d
Ken <kensanchez16@gmail.com>
2024-05-11 16:24:28 -0600

    Some seeds to test on local db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
+require "faker"
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.create( email: Faker::Internet.email,
+                    username: Faker::Internet.username,
+                    complete_name: "#{Faker::Name.name} #{Faker::Name.last_name}",
+                    birth_date: Faker::Date.birthday(min_age: 18,
+                    max_age: 65),
+                    phone: Faker::PhoneNumber.cell_phone,
+                    password: "12345678" )
+user.save!
+
+category = Category.create( name: Faker::Restaurant.type )
+category.save!
+
+food_stand = FoodStand.create( name: Faker::Restaurant.name,
+                               address: Faker::Address.full_address,
+                               hours_open: Faker::Time.between(from: DateTime.now - 1,to: DateTime.now).strftime("%H:%M"),
+                               hours_close: Faker::Time.between(from: DateTime.now,to: DateTime.now + 1).strftime("%H:%M"),
+                               latitude: Faker::Address.latitude,
+                               longitude: Faker::Address.longitude,
+                               user_id: user.id,
+                               category_id: Category.first.id )
+food_stand.save!
