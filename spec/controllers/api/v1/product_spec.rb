5724e2de80cef24361a8b27b96a6bbee12e6b60c
Ken <kensanchez16@gmail.com>
2024-05-11 23:49:51 -0600

    Fixed spec for products

--- spec/controllers/api/v1/product_spec.rb
+++ spec/controllers/api/v1/product_spec.rb
@@ -6,6 +6,7 @@ require 'byebug'
 RSpec.describe Api::V1::ProductsController, type: :controller do
 
   describe "POST #create" do
+    let(:user) { create(:user) }
     let(:food_stand) { create(:food_stand) }
     let(:product) { create(:product) }
 
@@ -23,7 +24,6 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
 
       it "renders a JSON response with the new product" do
         sign_in user
-
         post :create, params: { product: {
           name: product.name,
           price: product.price,
@@ -33,7 +33,20 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
         expect(response).to have_http_status(:ok)
         expect(response.content_type).to eq('application/json; charset=utf-8')
       end
-
     end
+
+    context "with invalid params" do
+      it "does not create a new Product" do
+        sign_in user
+
+        expect {
+          post :create, params: { product: { name: nil, price: nil } }
+        }.to change(Product, :count).by(0)
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.body).to include('errors')
+      end
+    end
+
   end
 end
