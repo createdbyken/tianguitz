39c23121c1ad83fd8b1b455752672ea56ee3e7ad
Ken <kensanchez16@gmail.com>
2024-05-11 23:41:49 -0600

    Added new create product controller

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::ProductsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: @product
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :food_stand_id)
+  end
+end
