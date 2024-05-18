064eca771e1f5fc20bc2aaa86e95a5bf340c806c
Ken <kensanchez16@gmail.com>
2024-05-18 00:05:55 -0600

    Show a product

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,6 +3,16 @@
 class Api::V1::ProductsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render json: @product
+  end
+
   def create
     @product = Product.new(product_params)
     if @product.save
@@ -11,6 +21,22 @@ class Api::V1::ProductsController < ApplicationController
       render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
     end
   end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    render json: { message: 'Product deleted' }
+  end
+
 
   private
 
