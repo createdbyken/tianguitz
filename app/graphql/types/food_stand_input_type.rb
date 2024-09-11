0a706957604a322d7cb651ec97b180962bad3a71
Ken <kensanchez16@gmail.com>
2024-09-10 19:43:48 -0600

    Added new column for is open on a food_Stand

--- app/graphql/types/food_stand_input_type.rb
+++ app/graphql/types/food_stand_input_type.rb
@@ -6,6 +6,7 @@ module Types
     argument :address, String, required: true
     argument :hours_open, String, required: true
     argument :hours_close, String, required: true
+    argument :is_open, Boolean, required: true
     argument :latitude, Float, required: true
     argument :longitude, Float, required: true
 
